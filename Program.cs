8e454365da21641986f7c25cc2fa232898ed9bb0
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-16 14:53:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.if_year__is_leap_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IfYearIsLeap(2016));
+            Console.WriteLine(IfYearIsLeap(2019));
+            Console.ReadLine();
+
+        }
+        static bool IfYearIsLeap(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+    }
+}
